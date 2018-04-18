63e1d09b477873d82eb6c6dd27a17db795ba1450
MscrmTools <tanguy92@hotmail.com>
2018-04-18 18:14:01 +0200

    Updated version numbers

--- McTools.Xrm.Connection/Properties/AssemblyInfo.cs
+++ McTools.Xrm.Connection/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2017.10.15")]
-[assembly: AssemblyFileVersion("1.2017.10.15")]
+[assembly: AssemblyVersion("1.2018.4.16")]
+[assembly: AssemblyFileVersion("1.2018.4.16")]
