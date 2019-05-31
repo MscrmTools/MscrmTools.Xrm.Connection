985b774286b249340968c4b5b51a69be8e7d9052
Shawn Lautebach <shawn.lautebach@mnp.ca>
2019-05-30 21:21:01 -0400

    Update to accept the interface of a IWin32Window

--- McTools.Xrm.Connection.WinForms/FormHelper.cs
+++ McTools.Xrm.Connection.WinForms/FormHelper.cs
@@ -7,9 +7,9 @@ namespace McTools.Xrm.Connection.WinForms
 {
     public class FormHelper
     {
-        private readonly ContainerControl innerAppForm;
+        private readonly IWin32Window innerAppForm;
 
-        public FormHelper(ContainerControl innerAppForm)
+        public FormHelper(IWin32Window innerAppForm)
         {
             this.innerAppForm = innerAppForm;
         }
